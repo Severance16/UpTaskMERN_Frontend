4122b9fcbbf960572d967407aab8a13647f1af96
Sergio Ortiz <sergio.380@outlook.com>
2023-08-26 21:41:08 -0500

    Creacion de area privada - componentes y context

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,34 @@ import OlvidePassword from "./paginas/OlvidePassword";
 import NuevoPassword from "./paginas/NuevoPassword";
 import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
 import Proyectos from "./paginas/proyectos";
+import NuevoProyecto from "./paginas/NuevoProyecto";
 
 import { AuthProvider } from "./context/AuthProvider";
+import { ProyectosProvider } from "./context/ProyectosProvider";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AuthLayouth />}>
-            <Route index element={<Login />} />
-            <Route path="registrar" element={<Registrar />} />
-            <Route path="olvide-password" element={<OlvidePassword />} />
-            <Route path="olvide-password/:token" element={<NuevoPassword />} />
-            <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
-          </Route>
-          
-          <Route path="/proyectos" element={<RutaProtegida/>}>
-            <Route index element={<Proyectos/>}/>
-          </Route>
-        </Routes>
+        <ProyectosProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayouth />}>
+              <Route index element={<Login />} />
+              <Route path="registrar" element={<Registrar />} />
+              <Route path="olvide-password" element={<OlvidePassword />} />
+              <Route
+                path="olvide-password/:token"
+                element={<NuevoPassword />}
+              />
+              <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+            </Route>
+
+            <Route path="/proyectos" element={<RutaProtegida />}>
+              <Route index element={<Proyectos />} />
+              <Route path="crear-proyecto" element={<NuevoProyecto />} />
+            </Route>
+          </Routes>
+        </ProyectosProvider>
       </AuthProvider>
     </BrowserRouter>
   );
