347ec0daa4cb0a79276c60e689a4736dda6d02a0
Sergio Ortiz <sergio.380@outlook.com>
2023-08-28 11:35:39 -0500

    Creación de proyectos

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -1,25 +1,65 @@
-import { useState, useEffect, createContext } from "react"
-import clienteAxios from "../config/clienteAxios"
+import { useState, useEffect, createContext } from "react";
+import clienteAxios from "../config/clienteAxios";
+import { useNavigate } from "react-router-dom"
 
-const ProyectosContext = createContext()
+const ProyectosContext = createContext();
 
-const ProyectosProvider = ({children}) => {
+const ProyectosProvider = ({ children }) => {
+  const [proyectos, setProyectos] = useState([]);
+  const [alerta, setAlerta] = useState({});
 
-    const [ proyectos, setProyectos ] = useState([])
+  const navigate = useNavigate()
 
-    return(
-        <ProyectosContext.Provider
-            value={{
-                proyectos
-            }}
-        >
-            {children}
-        </ProyectosContext.Provider>
-    )
-}
+  const mostrarAlerta = (alerta) => {
+    setAlerta(alerta);
 
-export {
-    ProyectosProvider
-}
+    setTimeout(() => {
+      setAlerta({});
+    }, 5000);
+  };
 
-export default ProyectosContext
+  const submitProyecto = async (proyecto) => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) return;
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        }
+      };
+      const { data } = await clienteAxios.post('/proyectos', proyecto, config)
+      console.log(data)
+
+      setAlerta({
+        msg: 'Proyecto Creado Correctamente',
+        error: false
+      })
+    } catch (error) {
+      console.log(error);
+    }
+
+    setTimeout(() => {
+      setAlerta({})
+      navigate('/proyectos')
+    },3000)
+  };
+
+  return (
+    <ProyectosContext.Provider
+      value={{
+        proyectos,
+        alerta,
+        mostrarAlerta,
+        submitProyecto,
+      }}
+    >
+      {children}
+    </ProyectosContext.Provider>
+  );
+};
+
+export { ProyectosProvider };
+
+export default ProyectosContext;
--- src/paginas/NuevoProyecto.jsx
+++ src/paginas/NuevoProyecto.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import FormularioProyecto from "../components/FormularioProyecto"
 
 const NuevoProyecto = () => {
   return (
     <>
-      <h1 className='text-4xl font-black'>Nuevo Proyecto</h1>
-      <div>
-        
+      <h1 className='text-4xl font-black'>Crear Proyecto</h1>
+      <div className='mt-10 flex justify-center'>
+        <FormularioProyecto/>
       </div>
     </>
   )
